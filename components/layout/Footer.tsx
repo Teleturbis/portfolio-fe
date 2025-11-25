5d060d8ddbf40a605a4039893f9285477ce249f0
Kevin Poppe <kevin.poppe93@gmail.com>
2025-11-25 16:03:42 +0100

    add privacy- and cookie links

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,4 +1,5 @@
 import { useTranslations } from 'next-intl';
+import Link from 'next/link';
 
 export function Footer() {
   const t = useTranslations('Footer');
@@ -10,6 +11,27 @@ export function Footer() {
         <p className='text-muted-foreground text-sm'>
           © {currentYear} {t('copyright')}
         </p>
+      </div>
+      <div className='mt-6 flex justify-center gap-4'>
+        {[
+          {
+            href: 'https://www.iubenda.com/privacy-policy/59196958',
+            title: 'Datenschutzerklärung',
+          },
+          {
+            href: 'https://www.iubenda.com/privacy-policy/59196958/cookie-policy',
+            title: 'Cookie-Richtlinie',
+          },
+        ].map((href, index) => (
+          <Link
+            href={href.href}
+            title={href.title}
+            key={index}
+            className='text-sm text-gray-400 transition-colors hover:text-gray-200'
+          >
+            {href.title}
+          </Link>
+        ))}
       </div>
     </footer>
   );
